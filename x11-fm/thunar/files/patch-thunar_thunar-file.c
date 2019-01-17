53819a98d0ba893bb30f4337a5060d1241ecd2d7
Guido Falsi <mad@madpilot.net>
2019-01-17 23:05:28 +0100

    Update patch.

--- x11-fm/thunar/files/patch-thunar_thunar-file.c
+++ x11-fm/thunar/files/patch-thunar_thunar-file.c
@@ -1,17 +1,6 @@
---- thunar/thunar-file.c.orig	2017-11-25 16:54:56 UTC
+--- thunar/thunar-file.c.orig	2018-09-26 16:40:34 UTC
 +++ thunar/thunar-file.c
-@@ -3966,7 +3966,9 @@ thunar_file_unwatch (ThunarFile *file)
- gboolean
- thunar_file_reload (ThunarFile *file)
- {
--  _thunar_return_val_if_fail (THUNAR_IS_FILE (file), FALSE);
-+  /* if the file has already been destroyed, break here */
-+  if (!THUNAR_IS_FILE (file))
-+      return FALSE;
- 
-   /* clear file pxmap cache */
-   thunar_icon_factory_clear_pixmap_cache (file);
-@@ -4090,11 +4092,11 @@ thunar_file_compare_by_name (const ThunarFile *file_a,
+@@ -4144,11 +4144,11 @@ thunar_file_compare_by_name (const ThunarFile *file_a,
  
    /* case insensitive checking */
    if (G_LIKELY (!case_sensitive))
